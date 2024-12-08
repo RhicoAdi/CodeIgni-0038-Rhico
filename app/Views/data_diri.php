5f616ca7492fdf274ecf26b49c304a7b2617e38a
RhicoAdi <rhicoadiwirakusuma@gmail.com>
2024-12-08 18:15:30 +0700

    codeigni

---
+++ app/Views/data_diri.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data diri Mahasiswa</title>
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+</head>
+<body>
+<div class="container mt-3">
+    <h1>Data diri Rhico Adi Wira Kusuma</h1>
+    <hr>    
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+            <div class="col-md-4">
+            <img src="<?= base_url('c:\Users\rhico\OneDrive\Dokumen\PROFIL .jpg') ?>" alt="Rhico Adi Wira Kusuma"class="img-fluid rounded-start">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h6>Nama            : Rhico Adi Wira Kusuma</h6>
+                <h6>Nim             : 23.240.0038</h6>
+                <h6>kelas           : 3P41A</h6>
+                <h6>Jenis Kelamin   : Laki-laki</h6>
+                <h6>Alamat          : Batang </h6>
+                <h6>No hp           : Tidak ada </h6>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <a href="<?= base_url('') ?>"></a>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html> 
